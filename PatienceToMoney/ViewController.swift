c29e7f8c2400ecf0be82d4945a33a3824aa7babf
Kyoya-prog <kyoyaprog@gmail.com>
2020-06-09 21:27:14 +0900

    Initial Commit

---
+++ PatienceToMoney/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PatienceToMoney
+//
+//  Created by 松山響也 on 2020/06/09.
+//  Copyright © 2020 Kyoya Matsuyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
