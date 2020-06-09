ee8c1a2b3d7182950c351c7e438d74ff38c4444b
Kyoya-prog <kyoyaprog@gmail.com>
2020-06-09 21:31:48 +0900

    Create initial view and viewController

--- PatienceToMoney/ViewController.swift
+++ PatienceToMoney/ViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let picture = UIImage(named: "money")
+        button.setImage(picture, for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 40.0
     }
+    
+    @IBAction func next(_ sender: Any) {
+    }
+    
 
 
 }
