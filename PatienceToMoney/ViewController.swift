e2e52e5d6c0e9a9048e93dc2e4ec6f0a72e8b484
Kyoya-prog <kyoyaprog@gmail.com>
2020-06-10 22:11:51 +0900

    create template of PatientToMoney

--- PatienceToMoney/ViewController.swift
+++ PatienceToMoney/ViewController.swift
@@ -7,21 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+   
     @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let picture = UIImage(named: "money")
-        button.setImage(picture, for: .normal)
+        button.setImage(picture, for: .application)
         button.layer.masksToBounds = true
         button.layer.cornerRadius = 40.0
+        
+        
+        
     }
     
     @IBAction func next(_ sender: Any) {
+        
+        Auth.auth().signInAnonymously(completion: nil)
+        if Auth.auth().currentUser?.uid != nil{
+        performSegue(withIdentifier: "toIndex", sender: nil)
+        }else{
+            
+        }
     }
+    
     
 
 
